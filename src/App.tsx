3fed7ffc4a7dab8debf062735d489595e92b90ce
Pritesh <priteshpmantri@gmail.com>
2023-02-22 15:00:17 +0530

    chakra theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 
+const theme = extendTheme({
+    fonts: {
+        heading: 'Merriweather',
+        body: 'Inter',
+    },
+});
+
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <ChakraProvider theme={theme}>
+            <RouterProvider router={router} />
+        </ChakraProvider>
+    );
 }
 
 export default App;
