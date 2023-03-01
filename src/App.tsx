efd7eb44f75338d879ed3f26809e75ef3549ba81
Pritesh <priteshpmantri@gmail.com>
2023-03-01 15:24:44 +0530

    tired of react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { theme } from './theme';
 
 function App() {
     return (
         <ChakraProvider theme={theme}>
-            <RouterProvider router={router} />
+            <RouterProvider router={createBrowserRouter([])} />
         </ChakraProvider>
     );
 }
