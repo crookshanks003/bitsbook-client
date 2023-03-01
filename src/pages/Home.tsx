52aa1e1bdc1bd4ee7c5324a9692b92531851e197
Pritesh <priteshpmantri@gmail.com>
2023-03-01 16:04:29 +0530

    nextjs 🚀

--- src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-import { Heading } from '@chakra-ui/react';
-
-export function Home() {
-    return <Heading>Home</Heading>;
-}
--- src/pages/Login.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading, Box } from '@chakra-ui/react';
-
-export function Login() {
-    return (
-        <Box mt={4}>
-            <Heading>Login</Heading>
-        </Box>
-    );
-}
